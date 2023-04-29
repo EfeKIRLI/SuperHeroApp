980f52eb678bb24c5cf9bc1ebd0fd1bf2f53b758
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2023-04-29 22:44:51 +0300

    Add existing project Search Button

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`
 
 const getHero = document.getElementById('getHeroBtn')
 const heroImageDiv = document.getElementById('heroImage')
+const searcBtn = document.getElementById('searchBtn')
+const searchElInput = document.getElementById('input').innerText
 
-const getSuperHero = (id,)=> { 
+const getRandomSuperHero = (id,name)=> { 
     fetch(`${BASE_URL}${id}`)
     .then(response => response.json())
     .then(json => {
@@ -15,10 +17,25 @@ const getSuperHero = (id,)=> {
         //  document.querySelector('body').innerHTML += `<img src ="${json.image.url}">` both of two works
         })
 }
+const getSearchedSuperHero = (name)=>{
+    //name 👉 base_url/search/batman
+    // id : 👉base_url/id
+    //https://www.superheroapi.com/api.php/10223569763528853/search/spider-man
+    // json.image.url
+    // json.results[0].image.url
+    console.log(searchElInput)
+    fetch(`${BASE_URL}/search/${name}`)
+    .then(response => response.json())
+    .then(json => {
+        
+        const hero = json.results[0]
+        console.log(hero)
+        heroImageDiv.innerHTML = `<img src ="${hero.image.url}">`
+    })
+}
+// getRandomSuperHero(60)
 
-// getSuperHero(60)
-
-// getHero.onclick = () => getSuperHero(RandNum)
+// getHero.onclick = () => getRandomSuperHero(RandNum)
 // const RandNum = Math.floor(Math.random()*732)+1
 
 // both of two fucntions works
@@ -26,12 +43,20 @@ const getSuperHero = (id,)=> {
 getHero.addEventListener('click',(e)=>{
     const RandNum = Math.floor(Math.random()*732)+1
     e.preventDefault()
-    getSuperHero(RandNum)
+    getRandomSuperHero(RandNum)
     
     
 })
 
+searcBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    getSearchedSuperHero('thanos')
+})
+
 console.log(Math.floor(Math.random()*732)+1) // get the hero randomly.
+
+
+// 07:08:30
 
 
 
